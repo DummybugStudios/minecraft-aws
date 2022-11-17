9f9e446cc370d1512049d0138e33a8e8dc16209d
Sid Mishra <sid.mishra@bjss.com>
2022-11-17 12:15:52 +0000

    Added ecs service creation

--- cdk/lib/backend-stack.ts
+++ cdk/lib/backend-stack.ts
@@ -2,8 +2,9 @@ import * as cdk from 'aws-cdk-lib';
 import {
   aws_s3 as s3,
   aws_iam as iam,
-  aws_s3_deployment as s3deploy,
   aws_lambda as lambda,
+  aws_ecs as ecs,
+  aws_ec2 as ec2,
 } from 'aws-cdk-lib'
 import { Construct } from 'constructs';
 
@@ -15,9 +16,48 @@ export class BackendStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
+    // Create ECS cluster
+    const ecsCluster = new ecs.Cluster(this, "miencraft-server-cluster")
+
+    // create ECS task
+    const ecsTask = new ecs.TaskDefinition(this, "minecraft-server-task", {
+      compatibility: ecs.Compatibility.EC2_AND_FARGATE,
+      cpu: "1024",
+      memoryMiB: "2048",
+    })
+
+    ecsTask.addContainer("minecraft-server-image", {
+      image: ecs.ContainerImage.fromRegistry("registry.hub.docker.com/marctv/minecraft-papermc-server:latest"),
+      environment: {
+        "MEMORYSIZE":'1.5G'
+      },
+      memoryReservationMiB: 1536, // 1.5GiG
+    })
+
+    // Create ECS Service and security groups
+
+    const ecsSecurityGroup = new ec2.SecurityGroup(this, "minecraft-security-group", {
+      vpc: ecsCluster.vpc,
+      description: "Security group used by the minecraft ecs service",
+      allowAllOutbound: true
+    })
+
+    ecsSecurityGroup.addIngressRule(
+      ec2.Peer.anyIpv4(), ec2.Port.tcp(25565), "Minecraft server"
+    )
+
+    new ecs.FargateService(this, "minecraft-server-service", {
+      taskDefinition: ecsTask,
+      cluster: ecsCluster,
+      assignPublicIp: true,
+      desiredCount: 0,
+      minHealthyPercent: 100,
+      maxHealthyPercent: 200,
+      securityGroups: [ecsSecurityGroup],
+      //TODO: enableExecuteCommand: true,
+    })
 
     // Create lambda function
-
     const apiFunctions = new lambda.Function(this, 'minecraft-status-function', {
       runtime: lambda.Runtime.NODEJS_16_X,
       handler: 'index.handler',
