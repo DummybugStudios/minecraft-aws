74d3be2ddca74b7f47b1f5b96beeb969f1d5bbea
Sid Mishra <sid.mishra@bjss.com>
2022-12-02 14:42:39 +0000

    removed everything from dist

--- web/webpack.config.js
+++ web/webpack.config.js
@@ -2,6 +2,8 @@
 const path = require('path')
 const webpack = require('webpack')
 const config = require("./cdkconfig.json")
+const HTMLWebpackPlugin = require("html-webpack-plugin")
+const CopyWebpackPlugin = require("copy-webpack-plugin")
 
 module.exports = {
     entry: './src/index.ts',
@@ -10,11 +12,17 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module:{
-        rules: [{
-            test: /\.ts$/,
-            use: 'ts-loader',
-            exclude: /node_modules/,
-        }],
+        rules: [
+            {
+                test: /\.ts$/,
+                use: 'ts-loader',
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.html$/,
+                use: 'html-loader'
+            }
+        ],
     },
     resolve: {
         extensions: ['.ts', '.js'],
@@ -23,6 +31,16 @@ module.exports = {
         new webpack.DefinePlugin({
             __API__: JSON.stringify(config.url),
             __DEV__: process.env.DEV || false,
+        }),
+        new HTMLWebpackPlugin({
+            template: 'src/index.html'
+        }),
+        new CopyWebpackPlugin({
+            patterns:[
+                {
+                    from: 'assets/favicon.ico',
+                }
+            ]
         })
     ],
 }
