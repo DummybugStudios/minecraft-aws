a35fc72dbb7abd052699f658d1c8027206b9d639
Sid Mishra <sid.mishra@bjss.com>
2022-12-02 13:47:24 +0000

    Improved frontend mock API

--- web/src/api/mock.ts
+++ web/src/api/mock.ts
@@ -1,16 +1,35 @@
 import { APIStatus, IApi } from "./IApi"; 
+
+async function wait(ms : number) : Promise<void> {
+        return new Promise((resolve)=>{
+        setTimeout(resolve, ms)
+    })
+}
+
 export default class MockAPI implements IApi {
 
+    status: string
+    ip: string
+    constructor() {
+        this.status = "Stopped",
+        this.ip = "90.90.90.90"
+    }
+
     async getServerStatus(): Promise<APIStatus> {
+        await wait(200);
         return {
-            status: "Running",
-            ip: "90.90.90.90"
+            status: this.status,
+            ip: this.ip
         }
     }
 
     async stopServer(): Promise<void> {
+        this.status = "Stopping"
+        setTimeout(()=>{this.status = "Stopped"}, 4000)
     }
 
     async startServer(): Promise<void> {
+        this.status = "Starting"
+        setTimeout(()=>{this.status="Running"}, 4000)
     }
 } 
